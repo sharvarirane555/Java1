7c749be96cf246128077004582f841c3e56a002c
sharvarirane555 <sharvarirane5@gmail.com>
2020-08-19 13:12:40 +0530

    Another deletion

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,6 @@ public static void main(String args[]){
 <<<<<<< HEAD
 System.out.println("Hi, my name is Sharvari and I am amazing.");
 =======
-System.out.println("Hi, my name is Sharvari.");
 System.out.println("I am 22 years old.");
 System.out.println("I am practising git branching.");
 >>>>>>> dc90f8a0ccc7a0cfaebc2d5bf86cc2089cc60ad6
