f16fcf3d6a8e9a503f9be8b139a4eb2ac2aca94c
sharvarirane555 <sharvarirane5@gmail.com>
2020-08-19 15:33:16 +0530

    Practice

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,19 +1,10 @@
 class HelloWorld1{
 public static void main(String args[]){
-<<<<<<< HEAD
 System.out.println("Hi, my name is Sharvari and I am amazing.");
-=======
 System.out.println("I am 22 years old.");
-<<<<<<< HEAD
 System.out.println("I am the master branch.");
-=======
 System.out.println("I am practising git branching.");
-<<<<<<< HEAD
 System.out.println("I like git.");
-=======
 System.out.println("I like git alot.");
->>>>>>> master
->>>>>>> testing
->>>>>>> dc90f8a0ccc7a0cfaebc2d5bf86cc2089cc60ad6
 }
 }
