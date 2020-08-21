e7001386cc2ade3ade01bf101aadd053eba90b33
sharvarirane555 <sharvarirane5@gmail.com>
2020-08-21 12:57:43 +0530

    123

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ class HelloWorld1{
 public static void main(String args[]){
 System.out.println("Hi, my name is Sharvari and I am amazing.");
 System.out.println("I am 22 years old.");
-System.out.println("I am the master branch.");
 System.out.println("I am practising git branching.");
 System.out.println("I like git.");
 System.out.println("I like git alot.");
