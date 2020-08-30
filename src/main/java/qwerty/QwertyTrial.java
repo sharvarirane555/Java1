722d4a8343d7d12401bdf7eb6e1ea22cae120e93
sharvarirane555 <sharvarirane5@gmail.com>
2020-08-30 15:20:47 +0530

    Adding jar file

---
+++ src/main/java/qwerty/QwertyTrial.java
@@ -0,0 +1,7 @@
+package qwerty;
+
+public class QwertyTrial {
+	public static void main(String[] args) {
+		System.out.println("This is a Maven project.\nIt will be build through Jenkins.\nIt will be uploaded on Nexus Repository.");
+	}
+}
